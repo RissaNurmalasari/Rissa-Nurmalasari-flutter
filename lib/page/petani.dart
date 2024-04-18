0e7b85be15418ae9efe967608f0a4929d2243f6b
RissaNurmalasari <145743717+RissaNurmalasari@users.noreply.github.com>
2024-04-18 16:39:45 +0800

    Add files via upload

---
+++ lib/page/petani.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld1/models/petani.dart';
+// import 'package:helloworld1/service/apiStatic.dart';
+
+
+class DatasScreen extends StatefulWidget {
+  const DatasScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DatasScreenState createState() => _DatasScreenState();
+}
+
+class _DatasScreenState extends State<DatasScreen> {
+  late Future<Petani> futurePetani;
+
+
+  @override
+  void initState() {
+    super.initState();
+    // futurePetani = ApiStatic.getPetani() as Future<Petani>;
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // title: 'Fetch Data Example',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      // ),
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: const Text('Fetch Data Example'),
+        // ),
+        body: Center(
+          child: FutureBuilder<Petani>(
+            future: futurePetani,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final petani = snapshot.data!;
+
+                return Text("${petani.nama}");
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+),
+);
+}
+}
